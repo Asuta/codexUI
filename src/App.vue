b5d5ccb4312aa3554597f08c48b11375e139e55f
Igor <igor.levochkin@xrtc.io>
2026-05-12 19:48:57 +0700

    Fix feedback diagnostics review findings
    (cherry picked from commit c9c1643c2c6cdf08a7da2d77fbfd0430b7e580c7)

--- src/App.vue
+++ src/App.vue
@@ -2015,13 +2015,14 @@ onMounted(() => {
   void refreshTerminalQuickCommands()
 })
 
-watch(visibleFeedbackErrors, (values) => {
-  for (const value of values) {
+watch(visibleFeedbackErrors, (values, oldValues) => {
+  values.forEach((value, index) => {
+    if (value === oldValues[index]) return
     const message = value.trim()
     if (message) {
       recordVisibleFailure(message)
     }
-  }
+  })
 })
 
 onUnmounted(() => {
